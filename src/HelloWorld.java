4a529a7c7d395f2964972d56c138b641088d3bb0
glebovichh@gmail.com <glebovichh@gmail.com>
2023-03-22 22:01:10 +0300

    init commit with .gitignore

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+
+    //public - видят все
+    //static - не нужен объект нового класса
+    //void - ничего нам не возвращает
+    //main - говорит, что это старт нашего приложения
+    public static void main(String[] args) {
+        byte byteExample = 3;
+        char letter = 'a';
+        int myLetter = 30;
+        float myFloatNumber = 2.5f;
+        double myDouble = 3.5;
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(myLetter);
+        System.out.println("Hello world!");
+    }
+}
