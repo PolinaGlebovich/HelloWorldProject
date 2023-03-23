c241bd0ba853e3e66e036c240a1d1789d053aed3
glebovichh@gmail.com <glebovichh@gmail.com>
2023-03-23 17:04:50 +0300

    My additional homework

---
+++ src/AddHomework.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class AddHomework {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String user = console.nextLine();
+        System.out.println("Hello " + user + "!");
+    }
+}
