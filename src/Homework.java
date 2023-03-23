22eeedd166f1db989aa276a5d65db299426d41f4
PolinaGlebovich <128374271+PolinaGlebovich@users.noreply.github.com>
2023-03-23 18:17:21 +0300

    Update Homework.java

--- src/Homework.java
+++ src/Homework.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
-
 public class Homework {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
+   
         System.out.println("This is my second homework!");
-
-// additional homework
-
-        String user = console.nextLine();
-        System.out.println("Hello " + user + "!");
+        
     }
 }
