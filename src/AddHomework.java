42f1a7ea7bd7d6637a465b3e156d73bb38cdd57b
glebovichh@gmail.com <glebovichh@gmail.com>
2023-03-27 22:02:11 +0300

    Lesson 3 classes

--- src/AddHomework.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class AddHomework {
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        String user = console.nextLine();
-        System.out.println("Hello " + user + "!");
-    }
-}
--- src/Homework.java
+++ src/Homework.java
@@ -4,10 +4,5 @@ public class Homework {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("This is my second homework!");
-
-// additional homework
-
-        String user = console.nextLine();
-        System.out.println("Hello " + user + "!");
     }
 }
