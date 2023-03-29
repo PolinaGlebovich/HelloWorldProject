c6e2967f84a71cccb6e9f33e3bb9ccc24a3d785f
glebovichh@gmail.com <glebovichh@gmail.com>
2023-03-29 21:59:10 +0300

    Lesson 4 classes

---
+++ src/by/glebovich/MethodsClasswork.java
@@ -0,0 +1,12 @@
+package by.glebovich;
+import java.util.Scanner;
+public class MethodsClasswork {
+    public static void main(String[] args) {
+        printMyName();
+    }
+    private static void printMyName() {      // Ctrl + Alt + M - create method
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+    }
+
+}
